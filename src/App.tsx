ef0b88b7643464d42e8deaa6cff9163d258f73a3
ialexalvarado <ialejandroalvarado@gmail.com>
2024-02-24 18:11:04 -0600

    colors, fonts and new component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
@@ -8,7 +6,7 @@ function App() {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg ui-colors-bg-disabled">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,13 +15,25 @@ function App() {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#">About Me</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Pricing</a>
+                <a className="nav-link" href="#">Resume</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Experience</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Expertise</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">My Projects</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Contact Me</a>
               </li>
             </ul>
           </div>
